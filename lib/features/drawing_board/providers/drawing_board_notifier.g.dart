e717a06534ebc08f99471e905932038d8d5ef134
Schooleo <lnnt190805@gmail.com>
2026-04-29 12:13:54 +0700

    feat: set up drawing board notifier to manage canvas state and preview logic

---
+++ lib/features/drawing_board/providers/drawing_board_notifier.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'drawing_board_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$drawingBoardNotifierHash() =>
+    r'b809111ecec11c5d26fb669e5847c09452f6a932';
+
+/// See also [DrawingBoardNotifier].
+@ProviderFor(DrawingBoardNotifier)
+final drawingBoardNotifierProvider =
+    AutoDisposeNotifierProvider<
+      DrawingBoardNotifier,
+      DrawingBoardState
+    >.internal(
+      DrawingBoardNotifier.new,
+      name: r'drawingBoardNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$drawingBoardNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$DrawingBoardNotifier = AutoDisposeNotifier<DrawingBoardState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
